a785498882078e12e934b6fd896a5aa3f8eb660e
tukuyet <103932488+tukuyet@users.noreply.github.com>
2023-08-28 15:42:38 +0700

    update image
    update image

--- Exam-EJB-master/Exam-EJB-master/src/main/java/com/example/testapi/rest/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.testapi.rest;
-
-public class EmployeeNotFoundException extends RuntimeException {
-    public EmployeeNotFoundException(Integer id) {
-        super("Không tìm thấy nhân viên có id: " + id);
-    }
-}
